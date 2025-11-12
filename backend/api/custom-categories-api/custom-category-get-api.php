7a06f7e6a22326e9aaafe3abea34af6d25f56cb1
matopohanka <133820328+matopohanka@users.noreply.github.com>
2025-11-12 07:11:51 +0100

    CRUD for expenses

---
+++ backend/api/custom-categories-api/custom-category-get-api.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type, Auth');
+
+require_once __DIR__ . '/../../class/class-custom-categories.php';
+require_once __DIR__ . '/../../class/class-auth.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+$auth = new Auth();
+$customCategories = new CustomCategories();
+
+$jwt = str_replace('Bearer ', '', $_SERVER['HTTP_AUTH'] ?? '');
+
+if ($auth->isUser($jwt)) {
+    $user_id = $auth->getUserId($jwt);
+    echo json_encode($customCategories->getAllCustomCategories($user_id));
+} else {
+    echo json_encode(['success' => false, 'message' => 'Permission denied']);
+}
+?>
